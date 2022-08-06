015e6aa724bb6631c94fa1dc0375baa033180761
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-08-06 11:01:22 -0400

    feature: block 1.19b, remove go get, prompt env vars

--- init.zsh
+++ init.zsh
@@ -31,7 +31,6 @@ p6df::modules::go::vscodes() {
 #
 # Function: p6df::modules::go::home::symlink()
 #
-#  Depends:	 p6_file p6_string
 #  Environment:	 GOPATH P6_DFZ_SRC_DIR
 #>
 ######################################################################
@@ -52,57 +51,16 @@ p6df::modules::go::home::symlink() {
 ######################################################################
 p6df::modules::go::langs() {
 
-  (
-    p6_dir_cd $P6_DFZ_SRC_DIR/syndbg/goenv
-    p6_git_p6_pull
-  )
+  p6_run_dir "$P6_DFZ_SRC_DIR/syndbg/goenv" "p6_git_p6_pull"
 
   # nuke the old one
   local previous=$(goenv install -l | tail -2 | head -1 | sed -e 's, *,,g')
   goenv uninstall -f $previous
 
   # get the shiny one
-  local latest=$(goenv install -l | grep -v 1.18 | tail -1 | sed -e 's, *,,g')
+  local latest=$(goenv install -l | grep 1.18 | tail -1 | sed -e 's, *,,g')
   goenv install $latest
   goenv global $latest
-  goenv rehash
-
-  go get -u -v github.com/mdempsky/gocode
-  go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
-  go get -u -v github.com/ramya-rao-a/go-outline
-  go get -u -v github.com/acroca/go-symbols
-  go get -u -v golang.org/x/tools/cmd/guru
-  go get -u -v golang.org/x/tools/cmd/gorename
-  go get -u -v github.com/fatih/gomodifytags
-  go get -u -v github.com/haya14busa/goplay/cmd/goplay
-  go get -u -v github.com/josharian/impl
-  go get -u -v github.com/tylerb/gotype-live
-  go get -u -v github.com/rogpeppe/godef
-  go get -u -v github.com/zmb3/gogetdoc
-  go get -u -v golang.org/x/tools/cmd/goimports
-  go get -u -v github.com/sqs/goreturns
-  go get -u -v winterdrache.de/goformat/goformat
-  go get -u -v golang.org/x/lint/golint
-  go get -u -v github.com/cweill/gotests/...
-  go get -u -v github.com/alecthomas/gometalinter
-  go get -u -v honnef.co/go/tools/...
-  go get -u -v github.com/mgechev/revive
-  go get -u -v github.com/sourcegraph/go-langserver
-  go get -u -v github.com/go-delve/delve/cmd/dlv
-  go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
-  go get -u -v github.com/godoctor/godoctor
-
-  go get -u -v golang.org/x/tools/cmd/godoc
-  go get -u -v github.com/stretchr/testify
-  go get -u -v github.com/golang/lint/golint
-  go get -u -v gopkg.in/check.v1
-
-  go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
-  goenv rehash
-  (
-    p6_dir_cd $P6_DFZ_SRC_DIR/github.com/golangci/golangci-lint/cmd/golangci-lint
-    go install -ldflags "-X 'main.version=$(git describe --tags)' -X 'main.commit=$(git rev-parse --short HEAD)' -X 'main.date=$(date)'"
-  )
   goenv rehash
 }
 
@@ -143,7 +101,6 @@ p6df::modules::go::prompt::init() {
 #  Args:
 #	dir -
 #
-#  Depends:	 p6_string
 #  Environment:	 DISABLE_ENVS GOENV_ROOT GOPATH HAS_GOENV
 #>
 ######################################################################
@@ -172,17 +129,15 @@ p6df::modules::go::goenv::init() {
 #  Returns:
 #	str - str
 #
-#  Depends:	 p6_string
-#  Environment:	 GOPATH GOROOT
+#  Environment:	 GOENV_ROOT GOPATH GOROOT
 #>
 ######################################################################
 p6_go_env_prompt_info() {
 
   local str
-  if ! p6_string_blank "$GOPATH"; then
-    str="gopath:\t\t  $GOPATH
+  str="goenv_root:\t  $GOENV_ROOT
+gopath:\t\t  $GOPATH
 goroot:\t\t  $GOROOT"
-  fi
 
   p6_return_str "$str"
 }
