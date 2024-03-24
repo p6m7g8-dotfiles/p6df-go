65f8cd5ab2371ac4b7ac9711733f6e23c17acb80
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-03-24 12:14:22 -0400

    chore: libify (#4)

--- init.zsh
+++ init.zsh
@@ -66,108 +66,18 @@ p6df::modules::go::langs() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::go::langs::pull()
+# Function: p6df::modules::go::init(_module, dir)
 #
-#  Environment:	 P6_DFZ_SRC_DIR
-#>
-######################################################################
-p6df::modules::go::langs::pull() {
-
-  p6_run_dir "$P6_DFZ_SRC_DIR/syndbg/goenv" "p6_git_p6_pull"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::go::langs::nuke()
-#
-#>
-######################################################################
-p6df::modules::go::langs::nuke() {
-
-  # nuke the old one
-  local previous=$(p6df::modules::go::goenv::latest::installed)
-  goenv uninstall -f $previous
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::go::langs::install()
-#
-#>
-######################################################################
-p6df::modules::go::langs::install() {
-
-  # get the shiny one
-  local latest=$(p6df::modules::go::goenv::latest)
-  goenv install $latest
-  goenv global $latest
-  goenv rehash
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::go::langs::packages()
-#
-#>
-######################################################################
-p6df::modules::go::langs::packages() {
-
-  go install github.com/spf13/cobra-cli@latest
-  go install github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
-  go install github.com/ramya-rao-a/go-outline@latest
-  go install github.com/cweill/gotests/...@latest
-  go install github.com/mgechev/revive@master
-
-  go install golang.org/x/tools/cmd/guru@latest
-  go install golang.org/x/tools/cmd/goimports@latest
-
-  go install honnef.co/go/tools/cmd/staticcheck@latest
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::go::goenv::latest()
-#
-#>
-######################################################################
-p6df::modules::go::goenv::latest() {
-
-  goenv install -l | p6_filter_select "1.2" | p6_filter_last "1" | p6_filter_spaces_strip
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::go::goenv::latest::installed()
-#
-#>
-######################################################################
-p6df::modules::go::goenv::latest::installed() {
-
-  goenv install -l | p6_filter_from_end "2" | p6_filter_spaces_strip
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::go::init()
+#  Args:
+#	_module -
+#	dir -
 #
 #  Environment:	 P6_DFZ_SRC_DIR
 #>
 ######################################################################
 p6df::modules::go::init() {
+  local _module="$1"
+  local dir="$2"
 
   p6df::core::lang::mgr::init "$P6_DFZ_SRC_DIR/syndbg/goenv" "go"
 
---
+++ lib/langs.sh
@@ -0,0 +1,72 @@
+# shellcheck shell=bash
+######################################################################
+#<
+#
+# Function: p6df::modules::go::langs::pull()
+#
+#  Environment:	 P6_DFZ_SRC_DIR
+#>
+######################################################################
+p6df::modules::go::langs::pull() {
+
+  p6_run_dir "$P6_DFZ_SRC_DIR/syndbg/goenv" "p6_git_p6_pull"
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::go::langs::nuke()
+#
+#>
+######################################################################
+p6df::modules::go::langs::nuke() {
+
+  # nuke the old one
+  local previous=$(p6df::modules::go::goenv::latest::installed)
+  goenv uninstall -f $previous
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::go::langs::install()
+#
+#>
+######################################################################
+p6df::modules::go::langs::install() {
+
+  # get the shiny one
+  local latest=$(p6df::modules::go::goenv::latest)
+  goenv install $latest
+  goenv global $latest
+  goenv rehash
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::go::langs::packages()
+#
+#>
+######################################################################
+p6df::modules::go::langs::packages() {
+
+  go install github.com/spf13/cobra-cli@latest
+  go install github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
+  go install github.com/ramya-rao-a/go-outline@latest
+  go install github.com/cweill/gotests/...@latest
+  go install github.com/mgechev/revive@master
+
+  go install golang.org/x/tools/cmd/guru@latest
+  go install golang.org/x/tools/cmd/goimports@latest
+
+  go install honnef.co/go/tools/cmd/staticcheck@latest
+
+  p6_return_void
+}
