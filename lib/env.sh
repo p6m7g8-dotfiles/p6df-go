91627d49d25bc2dea5f1553b4c5e9d1efb5ddc6a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-25 16:04:48 -0500

    chore: refactor new p6common api

--- lib/env.sh
+++ lib/env.sh
@@ -8,7 +8,7 @@
 ######################################################################
 p6df::modules::go::goenv::latest() {
 
-  goenv install -l | p6_filter_row_select "1.2" | p6_filter_row_last "1" | p6_filter_spaces_strip
+  goenv install -l | p6_filter_row_select "1.2" | p6_filter_row_last "1" | p6_filter_strip_spaces
 }
 
 ######################################################################
@@ -20,5 +20,5 @@ p6df::modules::go::goenv::latest() {
 ######################################################################
 p6df::modules::go::goenv::latest::installed() {
 
-  goenv install -l | p6_filter_row_from_end "2" | p6_filter_spaces_strip
+  goenv install -l | p6_filter_row_from_end "2" | p6_filter_strip_spaces
 }
